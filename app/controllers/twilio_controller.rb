7f0aac772bd98dbcfa8757fd34f613b746a0a5fa
lizzerdrix <liz@lizbaillie.com>
2014-09-21 12:57:32 -0400

    adds array of lyrics from Kiss From A Rose

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,2 +1,29 @@
 class TwilioController < ApplicationController
+
+  def new
+  end
+
+  def create
+    lyrics = ["There used to be a graying tower alone on the sea",
+"You became the light on the dark side of me",
+"Love remained a drug that's the high and not the pill",
+"But did you know that when it snows",
+"My eyes become large and the light that you shine can be seen.",
+"BAY-BEHHH",
+"I compare you to a kiss from a rose on the gray.",
+"Ooh",
+"The more I get of you the stranger it feels, yeahhh",
+"And now that your rose is in bloom, a light hits the gloom on the gray",
+"There is so much a man can tell you, so much he can say",
+"You remain my power, my pleasure, my pain",
+"To me you're like a growing addiction that I can't deny",
+"Won't you tell me is that healthy, baby?",
+"I've been kissed by a rose on the gray",
+"I've been kissed by a rose",
+"...And if I should fall along the way",
+"...been kissed by a rose on the gray.",
+"Yes I compare you to a kiss from a rose on the gray"
+]
+  end
+
 end
