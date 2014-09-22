185df116b68510b262e72b96ac14b69b05f768b0
lizzerdrix <liz@lizbaillie.com>
2014-09-22 08:34:07 -0400

    it works

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -66,8 +66,8 @@ class TwilioController < ApplicationController
       she and Booth do a slow dance to the song."""
     ]
 
-    account_sid = ENV["account_sid"] 
-    auth_token = ENV["auth_token"] 
+    account_sid = 'AC47a172ee7e3248ed5579fce1899c9849'#ENV["account_sid"] 
+    auth_token = '728da0623dbb4a56198a05df9cce3602'#ENV["auth_token"] 
  
     @client = Twilio::REST::Client.new account_sid, auth_token 
 
